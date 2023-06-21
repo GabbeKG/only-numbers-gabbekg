2f78412246ddfd9654d8d1d74d27c89089d2c71c
GabbeKG <el_sueco91@hotmail.com>
2023-06-21 23:21:26 +0200

    Cleaned code

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ My first NPM package that uses regex to remove everything except numbers.
 #Update 1.1.0
 
 -Adjusted code to include export of functions and restructureing code
+#Update 1.1.4
+-Refactored code
+
+#Update 1.1.7
+
+-Bug fixes
+The package now takes a string and returns only numbers if there are any, otherwise returns an empty string
--- only-numbers.js
+++ only-numbers.js
@@ -1,25 +1,5 @@
-function addEnterKeyListener() {
-  var onlyNumberInput = document.getElementById("only-num");
-  onlyNumberInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById("myBtn").click();
-    }
-  });
-}
-
-function str() {
-  let myString = document.getElementById("num").value;
+function numOnly(myString) {
   let res = myString.replace(/\D/g, "");
-  document.getElementById("num").value = res;
-  document.getElementById("name-label").innerHTML = "Clean number";
-  console.log(res);
-  document.body.innerHTML = document.body.innerHTML.replace(
-    /Dirty number/g,
-    "Cleaned number"
-  );
+  return res;
 }
-module.exports = {
-  addEnterKeyListener,
-  str,
-};
+module.exports = numOnly;
